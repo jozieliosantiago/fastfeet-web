c3121ceddcc8f2b0f7c649f44dc4bcb8e434d5d6
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-05-01 01:08:03 -0300

    redux config

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import Routes from './routes';
 import history from './services/history';
 
 import GlobalStyle from './styles/global';
+import store from './store';
 
 export default function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
---
+++ src/store/createStore.js
@@ -0,0 +1,6 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+
+export default (reducers, middlewares) => {
+  const enhancer = compose(applyMiddleware(...middlewares));
+  return createStore(reducers, enhancer);
+};
