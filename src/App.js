0a57ad761a39556facef9a5b00454a85a66b4f6c
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-04-30 22:11:08 -0300

    global styles

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
 
+import GlobalStyle from './styles/global';
+
 export default function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
